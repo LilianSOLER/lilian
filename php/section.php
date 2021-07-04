29dc511b68521139f44c5c8f3b5c64575869436d
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-04 14:33:41 +0200

    Changement de la section principale de l'index

--- php/section.php
+++ php/section.php
@@ -1,28 +1,19 @@
-
 <section id="section1">
-<div class="bloc_2">
-    <article id="article1">
-        <p>Bienvenue sur mon site</p>
-        <p>Je mettrai dessus de nombreuses informations, tels que les corrig&eacute;s des plusieurs de mes
-            cours, ou bien des programmes divers</p>
-        <p>Notamment les corrections des td d'Arduino et d'intro au web de Peip 2.</p>
-        <p>Je mets aussi les exercises que je donne &agrave; mes &eacute;leves en cours particuliers.</p>
-        <br />
-        <p>Pour toute question, cliquer sur le pop-up en bas &agrave; droite de la page.</p>
-        <br />
-    </article>
-</div>
-
-<div class="bloc_2">
-    <article id="article2">
-        <br />
-        <p>Welcome to my website</p>
-        <p>I would put a lot of information on it, such as the answers to several of my courses, or various
-            programs</p>
-        <p>In particular the corrections of the Arduino td and the intro to the web of Peip 2.</p>
-        <p>I also put the exercises that I give to my students in private lessons.</p>
-        <br />
-        <p>For any questions, click on the pop-up at the bottom right of the page.</p>
-    </article>
-</div>
+    <div class="bloc_2">
+        <article id="article1">
+            <p>Bienvenue sur le site didelo.fr : </p>
+            <p>Lilian Soler, je suis un developpeur web junior orienté Back-End PHP</p>
+            <p>Je mettrais ici certains de mes projets de programmations, ainsi que des corrections des mes travaux scolaires</p>
+            <p>Pour toute demande de devis, veuillez vous adressez à cette adresse mail : <a href="mailto:liliansoler@didelo.fr">liliansoler@didelo.fr</a></p>
+        </article>
+    </div>
+    <br>
+    <div class="bloc_2">
+        <article id="article2">
+            <p>Welcome to the website didelo.fr :</p>
+            <p>Lilian Soler, I am a junior web developer focused on Back-End PHP</p>
+            <p>I would put here some of my programming projects, as well as corrections of my school work</p>
+            <p>For any quote request, please contact this email address:<a href="mailto:liliansoler@didelo.fr">liliansoler@didelo.fr</a></p>
+        </article>
+    </div>
 </section>
