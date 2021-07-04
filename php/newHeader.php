a0271e06f180c5c4e243af9121b313ddfb06565d
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-04 14:37:51 +0200

    Retouche du menu

--- php/newHeader.php
+++ php/newHeader.php
@@ -1,17 +1,17 @@
 <header>
   <div class="header">
-    <a href="index.php" class="logo">SOLER Lilian</a>
+    <a href="https://didelo.fr" class="logo">SOLER Lilian</a>
     <div class="header-right">
       <div class="topnav" id="myTopnav">
-        <a href="index.php" class="active">Accueil</a>
+        <a href="https://didelo.fr" class="active">Accueil</a>
         <div class="dropdown">
           <button class="dropbtn">Correction PEIP 2
             <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content">
-            <a title="Arduino: Programmes Divers et Corrections des TD" href="peip2/arduino/">Arduino</a>
-            <a title="Introduction au Web (HTML et CSS) : Correcttions des TD" href="peip2/web/">Intro au Web</a>
-            <a title="Applications du Web (PHP et JS) : Correcttions des TD" href="peip2/applicationWeb/">Applications du Web</a>
+            <a title="Arduino: Programmes Divers et Corrections des TD" href="https://didelo.fr/peip2/arduino/">Arduino</a>
+            <a title="Introduction au Web (HTML et CSS) : Correcttions des TD" href="https://didelo.fr/peip2/web/">Intro au Web</a>
+            <a title="Applications du Web (PHP et JS) : Correcttions des TD" href="https://didelo.fr/peip2/applicationWeb/">Applications du Web</a>
           </div>
         </div>
         <div class="dropdown">
@@ -28,10 +28,10 @@
             <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content">
-            <a title="Cours particuliers pour Amandine" href="cours/cours_amandine.php">Amandine</a>
-            <a title="Cours particuliers pour Salome" href="cours/cours_salome.php">Salom&eacute;</a>
-            <a title="Cours particuliers pour Slimane" href="cours/cours_slimane.php">Slimane</a>
-            <a title="Ressources pour les cours particuliers" href="cours/ressource.php">Ressources</a>
+            <a title="Cours particuliers pour Amandine" href="https://didelo.fr/cours/cours_amandine.php">Amandine</a>
+            <a title="Cours particuliers pour Salome" href="https://didelo.fr/cours/cours_salome.php">Salom&eacute;</a>
+            <a title="Cours particuliers pour Slimane" href="https://didelo.fr/cours/cours_slimane.php">Slimane</a>
+            <a title="Ressources pour les cours particuliers" href="https://didelo.fr/cours/ressource.php">Ressources</a>
           </div>
         </div>
         <div class="dropdown">
@@ -39,13 +39,13 @@
             <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content">
-            <a title="Entrainement au code en HTML et CSS" href="entrainement/_html/">HTML et CSS</a>
-            <a title="Entrainement au code CSS et ses animations" href="entrainement/_css/">Animation CSS</a>
-            <a title="Entrainement au code en PHP et MyQSL" href="entrainement/_php/">PHP et MyQSL</a>
-            <a title="Entrainement au code en PHP orienté objet" href="entrainement/_php_poo">PHP orienté objet</a>
-            <a title="Entrainement au code en JavaScript" href="entrainement/_js/">JavaScript</a>
-            <a title="Entrainement au Framework Bootstrap" href="entrainement/_bootstrap/">Bootstrap</a>
-            <a title="Différents projets d'entraînement" href="projet/">Diff&eacute;rents projets d'entraînement</a>
+            <a title="Entrainement au code en HTML et CSS" href="https://didelo.fr/entrainement/_html/">HTML et CSS</a>
+            <a title="Entrainement au code CSS et ses animations" href="https://didelo.fr/entrainement/_css/">Animation CSS</a>
+            <a title="Entrainement au code en PHP et MyQSL" href="https://didelo.fr/entrainement/_php/">PHP et MyQSL</a>
+            <a title="Entrainement au code en PHP orienté objet" href="https://didelo.fr/entrainement/_php_poo">PHP orienté objet</a>
+            <a title="Entrainement au code en JavaScript" href="https://didelo.fr/entrainement/_js/">JavaScript</a>
+            <a title="Entrainement au Framework Bootstrap" href="https://didelo.fr/entrainement/_bootstrap/">Bootstrap</a>
+            <a title="Différents projets d'entraînement" href="https://didelo.fr/projet/">Diff&eacute;rents projets d'entraînement</a>
           </div>
         </div>
         <div class="dropdown">
@@ -53,7 +53,7 @@
             <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content">
-            <a title="Curriculum Vitae  long de SOLER Lilian" href="media/image/cv.pdf">CV long </a>
+            <a title="Curriculum Vitae  long de SOLER Lilian" href="https://didelo.fr/media/image/cv.pdf">CV long </a>
             <a title="Curriculum Vitae court de SOLER Lilian" href="https://liliansoler.didelo.fr">CV court</a>
           </div>
         </div>
