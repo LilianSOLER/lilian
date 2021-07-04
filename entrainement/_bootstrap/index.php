ae27e0ae8c1105be15aec0c346f345172083bdd9
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-04 11:11:21 +0200

    Ajout images bootstrap et changement liens vers images dans toutes la partie bootstrap

---
+++ entrainement/_bootstrap/index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+        
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Entrainement au framework Bootstrap</title>
+        
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/_index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php include("../../php/script.php"); ?>
+</head>
+
+<body>
+    <div class='tout'>
+        <?php include("../../php/newHeader.php"); ?>
+        <div class="container">
+            <div class='row'>
+                <div class='col'>
+                    <h2>Partie 1: Créer des mises en page responsives</h2>
+                    <ul>
+                        <li><a href=partie1/grille.html>Décomposer la structure des pages</a></li>
+                        <li><a href=partie1/grille_responsive.html>Render votre mise en page responsive</a></li>
+                    </ul>
+                </div>
+                <div class='col'>
+                    <h2>Partie 2: Utiliser les composants d'interface utilisateur de Bootstrap pour afficher le contenu</h2>
+                    <ul>
+                        <li><a href=partie2/navigation.html>Ajouter des composants d'interface utilisateur (navigation)</a></li>
+                        <li><a href=partie2/navigation_responsive.html>Ajouter des composants d'interface utilisateur (navigation responsive)</a></li>
+                        <li><a href=partie2/jumbotron.html>Ajouter des composants d'interface utilisateur (jumbotron)</a></li>
+                        <li><a href=partie2/cartes.html>Ajouter des composants d'interface utilisateur (cartes)</a></li>
+                        <li><a href=partie2/footer.html>Ajouter des éléments typographiques à votre page (footer)</a></li>
+                        <li><a href=partie2/card-images.html>Ajouter des supports à votre page (cartes)</a></li>
+                        <li><a href=partie2/logo.html>Ajouter des supports à votre page (logo)</a></li>
+                        <li><a href=partie2/video.html>Ajouter des supports à votre page (video)</a></li>
+                        <li><a href=partie2/lessons6.html>Ajouter des supports à votre page (video 2)</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col'>
+                    <h2>Partie 3 : Ajouter de l'interactivité à votre site </h2>
+                    <ul>
+                        <li><a href=partie3/bouton.html>Ajoutez des composants interactifs (bouton)</a></li>
+                        <li><a href=partie3/boite.html>Le modèle des boites</a></li>
+                        <li><a href=partie3/flexbox.html>Mise en page avec Flexbox</a></li>
+                        <li><a href=partie3/blog_trotter.html>Pratique 1: Le blog trotter</a>
+                            </h2>
+                        <li><a href=partie3/vielles_facon.html>Autres techniques de mise en
+                                page(vielles façon)</a></li>
+                        <li><a href=partie3/zozor2.html>Pratique 2 : ZOZOR carnet de
+                                voyages</a></li>
+                        <li><a href=partie3/cv3.html>Pratique 3 : CV</a></li>
+                    </ul>
+                </div>
+                <div class='col'>
+                    <h2>Partie 4 : Fonctionnalités évoluées </h2>
+                    <ul>
+                        <li><a href=partie4/tableau.html>Ajouter des Tableaux</a></li>
+                        <li><a href=partie4/formulaire.html>Créer des formulaires</a></li>
+                        <li><a href=partie4/video_audio.html>Enrichir avec de la vidéo et de
+                                l'audio</a></li>
+                        <li><a href=partie4/media_queries1.html>Utiliser le reponsive design
+                                avec les Media Queries 1 </a></li>
+                        <li><a href=partie4/media_queries2.html>Utiliser le responsive design
+                                avec les Media Queries 2 </a></li>
+                        <li><a href=partie4/cv4.html>Pratique : CV</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+    <?php include("../../php/newSocial.php"); ?>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" async src="//ad.adxcore.com/adjs_r.php?async&what=zone:234867&inf=no"></script>
+</body>
+
+</html>
