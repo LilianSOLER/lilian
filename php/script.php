1fe06e9693ae09f1c2d1f4a1df1f92e9c255ceb8
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-04 10:36:57 +0200

    Ajout du fichier site.webmanifest et des icons ainsi qu'un changement dans script.php pour relier ces nouveaux à tous les pages du site

--- php/script.php
+++ php/script.php
@@ -32,3 +32,8 @@
 
   gtag('config', 'G-5714Y07X47');
 </script>
+
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
