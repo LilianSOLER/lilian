4fe44c0abfb8ccabf609623bbd90934354a9ac80
Lilian SOLER <soler.lilian64@gmail.com>
2021-07-04 11:27:13 +0200

    Ajout des fichiers css de la partie css

---
+++ entrainement/_css/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entrainement au CSS et ses animations :</title>
+    <link rel="stylesheet" type="text/css" href="css/_index.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <?php include("../../php/script.php"); ?>
+</head>
+
+<body>
+    <div class='tout'>
+        <?php include("../../php/newHeader.php"); ?>
+        <div class="html">
+            <h1>Entrainement au code CSS et ses animations :</h1>
+
+            <ul>
+                <li>Partie 1: Réalisez vos premières animations CSS</li>
+                <ul class='sous-html'>
+                    <li><a href=partie1/moustached.php>Découvrez les animations web : Moustached-Nanny</a></li>
+                    <li><a href=partie1/transition.php>Créez des animations simples avec les transitions</a></li>
+                </ul>
+            </ul>
+        </div>
+
+        <?php include("../../php/newSocial.php"); ?>
+        <!-- 728x90_btf  Leader board-->
+        <ins data-zone="234867" class="byadthink"></ins>
+        <script type="text/javascript" async src="//ad.adxcore.com/adjs_r.php?async&what=zone:234867&inf=no"></script>
+
+    </div>
+</body>
+
+</html>
---
+++ entrainement/_css/partie1/transition.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Créez des animations simples avec les transitions :</title>
+    <link rel="stylesheet" type="text/css" href="../css/_index.css">
+    <link rel="stylesheet" type="text/css" href="../css/partie1/transition.css">
+    <?php include("../../../php/script.php"); ?>
+</head>
+
+<body>
+    <div class='tout'>
+        <?php include("../../../php/newHeader.php"); ?>
+        <div class="html">
+
+            <div class="container">
+                <div class="btn">
+                    Pourquoi tu ne bouge pas !?
+                </div>
+            </div>
+            </br>
+            <div class="container">
+                <div class="btn1">
+                    Je veux de la fluidité
+                </div>
+            </div>
+            </br>
+            <div class="container">
+                <div class="btn2">
+                    Je grandis lentement mais sûrement
+                </div>
+            </div>
+            </br>
+            <div class="container">
+                <div class="btn3">
+                    Vois comme je grandis !
+                </div>
+            </div>
+            </br>
+
+        </div>
+
+        <?php include("../../../php/newSocial.php"); ?>
+        <!-- 728x90_btf  Leader board-->
+        <ins data-zone="234867" class="byadthink"></ins>
+        <script type="text/javascript" async src="//ad.adxcore.com/adjs_r.php?async&what=zone:234867&inf=no"></script>
+
+    </div>
+</body>
+
+</html>
