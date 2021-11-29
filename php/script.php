5523d03c79bb8e6a627e7f42a53de4613c2a64ad
SOLER Lilian <soler.lilian64@gmail.com>
2021-11-29 10:07:34 +0100

    Ajout du lien vers les icones font-awesome

--- php/script.php
+++ php/script.php
@@ -40,6 +40,10 @@
   gtag('config', 'G-5714Y07X47');
 </script>
 
+<link 
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" 
+  rel="stylesheet"  type='text/css'>
+
 <link rel="apple-touch-icon" sizes="180x180" href="https://<?php echo $ndd; ?>/lilian/media/icons/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="https://<?php echo $ndd; ?>/lilian/media/icons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="https://<?php echo $ndd; ?>/lilian/media/icons/favicon-16x16.png">
