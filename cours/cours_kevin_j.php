7cbf76c5236e3be1400573332c2b74b9151f708e
SOLER Lilian <soler.lilian64@gmail.com>
2022-01-24 20:27:45 +0100

    Création de la page de cours de Kevin Juppet et ajout du cours(et de son fichier) et du lien vers les exercises du jour

---
+++ cours/cours_kevin_j.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cours particuliers pour Kevin;</title>
+    <link rel="stylesheet" type="text/css" href="css/_index.css">
+    <link rel="stylesheet" type="text/css" href="css/cours.css">
+    <?php include("../php/script.php"); ?>
+</head>
+
+<body>
+    <div class='tout'>
+        <?php include("../php/newHeader.php"); ?>
+        <div class="html">
+            <h1> Kevin </h1>
+
+            <ul>
+                <li>Mathématiques :</li>
+                <ul class='sous-html'>
+                    <li><a href=media/image/cours_24_01_kevin_j.pdf>Cours Nombre décimaux(après séance du 24/01)</a></li>
+                    <li><a href=https://forms.gle/6iPu3JCByMBztR9t8>Exercise calcul nombre décimaux(après séance du 24/01)</a></li>
+                </ul>
+            </ul>
+        </div>
+
+        <?php include("../php/newSocial.php"); ?>
+    </div>
+</body>
+
+</html>
