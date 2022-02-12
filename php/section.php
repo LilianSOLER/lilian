59b629f9e1e1435616a0ea0c8d33a721a5cac399
SOLER Lilian <soler.lilian64@gmail.com>
2022-02-12 12:15:24 +0100

    Modification de la section principale de l'acceuil

--- php/section.php
+++ php/section.php
@@ -2,18 +2,18 @@
     <div class="bloc_2">
         <article id="article1">
             <p>Bienvenue sur le site didelo.fr : </p>
-            <p>Lilian Soler, je suis un developpeur web junior orienté Back-End</p>
-            <p>Je mettrais ici certains de mes projets de programmations, ainsi que des corrections des mes travaux scolaires</p>
-            <p>Pour toute demande de devis, veuillez vous adressez à cette adresse mail : <a href="mailto:lilian.soler@didelo.fr">lilian.soler@didelo.fr</a></p>
+            <p>Lilian Soler, je suis étudiant en école d'ingénieur ,developpeur web junior orienté Back-End, professeur indépendant en mathématiques et informatiques, et professeur pour Acadomia</p>
+            <p>Je met ici certains de mes projets de programmations, des corrections des mes travaux scolaires mais je me sert de ce site comme une plateforme pour mes élèves de cours particuliers</p>
+            <p>Pour toute demande de devis ou de cours particulier, veuillez vous adressez à cette adresse mail : <a href="mailto:lilian.soler@didelo.fr">lilian.soler@didelo.fr</a></p>
         </article>
     </div>
     <br>
     <div class="bloc_2">
         <article id="article2">
             <p>Welcome to the website didelo.fr :</p>
-            <p>Lilian Soler, I am a junior web developer focused on Back-End</p>
-            <p>I would put here some of my programming projects, as well as corrections of my school work</p>
-            <p>For any quote request, please contact this email address: <a href="mailto:lilian.soler@didelo.fr">lilian.soler@didelo.fr</a></p>
+            <p>Lilian Soler, I am an engineering school student, Back-End oriented junior web developer, freelance professor in mathematics and computer science, and professor for Acadomia</p>
+            <p>here some of my programming projects, corrections of my school work but I use this site as a platform for my private lessons</p>
+            <p>For any quote request or a private lesson, please contact this email address: <a href="mailto:lilian.soler@didelo.fr">lilian.soler@didelo.fr</a></p>
         </article>
     </div>
 </section>
