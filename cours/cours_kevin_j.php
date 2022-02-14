d7b4e92fb656835b56c0138e3df7ebbe17e02fde
Lilian SOLER <soler.lilian64@gmail.com>
2022-02-14 19:49:47 +0100

    Ajout du cours du jour de Kevin

--- cours/cours_kevin_j.php
+++ cours/cours_kevin_j.php
@@ -21,9 +21,10 @@
                 <ul class='sous-html'>
                     <li><a href=media/image/cours_24_01_kevin_j.pdf>Cours Nombre décimaux(après séance du 24/01)</a></li>
                     <li><a href=https://forms.gle/6iPu3JCByMBztR9t8>Exercise calcul nombre décimaux(après séance du 24/01)</a></li>
-                    <li><a href=media/image/cours_07_02_kevin_j.pdf>Cours Angle et Correction DS(après séance du 07/01)</a></li>
-                    <li><a href="media/image/exo_operations_6">Exercice Opérations(après séance du 07/01)</a></li>
-                    <li><a href="media/image/exo_mesure_angle_6.pdf">Exercice mesure angle(après séance du 07/01)</a></li>
+                    <li><a href=media/image/cours_07_02_kevin_j.pdf>Cours Angle et Correction DS(après séance du 07/02)</a></li>
+                    <li><a href="media/image/exo_operations_6">Exercice Opérations(après séance du 07/02)</a></li>
+                    <li><a href="media/image/exo_mesure_angle_6.pdf">Exercice mesure angle(après séance du 07/02)</a></li>
+                    <li><a href="media/image/cours_14_02_kevin_j.pdf">Cours Opération(après séance du 14/02)</a></li>
                     <li><a href="rendu_exercice.php">Rendu Des Exercices</a></li>
                 </ul>
             </ul>
