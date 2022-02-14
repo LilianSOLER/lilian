f2186df217b807d1ccf278b6a63870b82987442d
SOLER Lilian <soler.lilian64@gmail.com>
2022-02-14 20:21:56 +0100

    Mise en pied de page des scripts js

--- php/script.php
+++ php/script.php
@@ -9,34 +9,7 @@
 <meta name="geo.region" content="FR-FR" />
 
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-  var Tawk_API = Tawk_API || {},
-    Tawk_LoadStart = new Date();
-  (function() {
-    var s1 = document.createElement("script"),
-      s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/5f9acad97f0a8e57c2d8348d/default';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-  })();
-</script>
-<!--End of Tawk.to Script-->
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5714Y07X47"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-
-  gtag('config', 'G-5714Y07X47');
-</script>
 
 <link rel="apple-touch-icon" sizes="180x180" href="https://didelo.fr/lilian/media/icons/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="https://didelo.fr/lilian/media/icons/favicon-32x32.png">
@@ -46,7 +19,3 @@
 <link 
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" 
   rel="stylesheet"  type='text/css'>
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5714Y07X47"></script>
-
-<script src="https://unpkg.com/typeit@8.2.0/dist/index.umd.js"></script>
