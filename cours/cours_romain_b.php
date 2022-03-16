54190193403b233c8c66f8970c59df67f0c746a1
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-16 17:10:13 +0100

    Ajout de la page de cours romain ainsi que son accès via le menu

---
+++ cours/cours_romain_b.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cours particuliers pour Romain</title>
+    <link rel="stylesheet" type="text/css" href="css/_index.css">
+    <link rel="stylesheet" type="text/css" href="css/cours.css">
+    <?php include("../php/script.php"); ?>
+</head>
+
+<body>
+    <div class='tout'>
+        <?php include("../php/newHeader.php"); ?>
+        <div class="html">
+            <h1> Romain </h1>
+
+            <ul>
+                <li>Mathématiques :</li>
+                <ul class='sous-html'>
+                    <li><a href=media/image/cours_24_01_kevin_j.pdf>Cours Nombre décimaux(après séance du 5/03)</a></li>
+                </ul>
+            </ul>
+        </div>
+
+        <?php include("../php/newSocial.php"); ?>
+        <?php include("../php/script_bottom.php"); ?>
+    </div>
+</body>
+
+</html>
--- php/newHeader.php
+++ php/newHeader.php
@@ -33,6 +33,7 @@
             <a title="Cours particuliers pour Salome" href="https://didelo.fr/lilian/cours/cours_salome.php">Salom&eacute;</a>
             <a title="Cours particuliers pour Slimane" href="https://didelo.fr/lilian/cours/cours_slimane.php">Slimane</a>
             <a title="Cours particuliers pour Kevin" href="https://didelo.fr/lilian/cours/cours_kevin_j.php">Kevin</a>
+            <a title="Cours particuliers pour Kevin" href="https://didelo.fr/lilian/cours/cours_romain_b.php">ROmain</a>
             <a title="Ressources pour les cours particuliers" href="https://didelo.fr/lilian/cours/ressource.php">Ressources</a>
           </div>
         </div>
