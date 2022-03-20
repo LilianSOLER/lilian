ec8a5eb9bbb539eace38ed345fee9da9a32e1728
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-20 15:24:47 +0100

    MAJ grid index

--- info3/pw6/index.php
+++ info3/pw6/index.php
@@ -15,34 +15,41 @@
         <?php include("../../php/newHeader.php"); ?>
         <div class="html">
             <h1>Correction TD de Programmation Web : </h1>
-            <ul>
-                <li>TD n°1 : Exercices PHP </li>
-                <ul class='sous-html'>
-                    <li><a href="../../../PW6/Exercices/TP1/calcul.html">Calcul d'intérêts composés (niv. 1-2)</a></li>
-                    <li><a href="../../../PW6/Exercices/TP1/">Un peu de style en CSS (niv. 1)</a></li>
-                    <li><a href="../../../PW6/Exercices/TP1/multiplication.php">Table de multiplication (niv. 1-2)</a></li>
-                    <li><a href="../../../PW6/Exercices/TP1/unicode.php">Analyse des Caractères Unicode (niv. 2)</a></li>
-                    <li><a href="../../../PW6/Exercices/TP1/multiplication_with_generic_table.php">Affichage d'un tableau générique (niv. 2)</a></li>
-                    <li><a href="../../../PW6/Exercices/TP1/form_agenda.html">Calendrier - agenda web (niv. 3)</a></li>
-                    <li><a href="../../../PW6/Exercices/TP1/form_agenda_with_generic_table.php">Calendrier - agenda web (niv. 3)</a></li>
-                    <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
-                </ul>
-            </ul>
-            <p></p>
-            <ul>
-                <li>TD n°2 : </li>
-                <ul class='sous-html'>
-                    <li><a href="../../../PW6/Exercices/TP2/compte-rendu.md">Compte Rendu</a></li>
-                    <li><a href="../../../PW6/Exercices/TP2/Point_Acces_Wifi/comptage.php">Points d'accès wifi</a></li>
-                    <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
-                </ul>
-                <p></p>
-            </ul>
-            <ul>
-                <li>TD n°3 : </li>
-                <ul class='sous-html'>
-                <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
-                </ul>
+            <ul class="grille">
+                <div class="div1">
+                    <li>TD n°1 : Exercices PHP </li>
+                    <ul class='sous-html'>
+                        <li><a href="../../../PW6/Exercices/TP1/calcul.html">Calcul d'intérêts composés (niv. 1-2)</a></li>
+                        <li><a href="../../../PW6/Exercices/TP1/">Un peu de style en CSS (niv. 1)</a></li>
+                        <li><a href="../../../PW6/Exercices/TP1/multiplication.php">Table de multiplication (niv. 1-2)</a></li>
+                        <li><a href="../../../PW6/Exercices/TP1/unicode.php">Analyse des Caractères Unicode (niv. 2)</a></li>
+                        <li><a href="../../../PW6/Exercices/TP1/multiplication_with_generic_table.php">Affichage d'un tableau générique (niv. 2)</a></li>
+                        <li><a href="../../../PW6/Exercices/TP1/form_agenda.html">Calendrier - agenda web (niv. 3)</a></li>
+                        <li><a href="../../../PW6/Exercices/TP1/form_agenda_with_generic_table.php">Calendrier - agenda web (niv. 3)</a></li>
+                        <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
+                    </ul>
+                </div>
+                <div class="div2">
+                    <li>TD n°2 : Service Web </li>
+                    <ul class='sous-html'>
+                        <li><a href="../../../PW6/Exercices/TP2/compte-rendu.md">Compte Rendu</a></li>
+                        <li><a href="../../../PW6/Exercices/TP2/Point_Acces_Wifi/comptage.php">Points d'accès wifi</a></li>
+                        <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
+                    </ul>
+                </div>
+                <div class="div3">
+                    <li>TD n°3 : Podcasts- TMBD</li>
+                    <ul class='sous-html'>
+                        <li><a href="../../../PW6/Exercices/TP3/compte-rendu.md">Compte Rendu</a></li>
+                        <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
+                    </ul>
+                </div>
+                <div class="div4">
+                    <li>TD n°4 : ???</li>
+                    <ul class='sous-html'>
+                        <li><a href="#">??</a></li>
+                    </ul>
+                </div>
             </ul>
         </div>
     </div>
