50ab74d7c06d25b18d3a20ab4eb88e809e7b440c
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-21 16:24:12 +0100

    Déboggage

--- info3/pw6/compte_rendu.php
+++ info3/pw6/compte_rendu.php
@@ -1,31 +1,34 @@
 <?php
-require_once '../../php/php-markdown-lib/Michelf/Markdown.inc.php';
-  spl_autoload_register(function($class){
-    require str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
-  });
-  use Michelf\Markdown;
-  $path = "../../../PW6"; //path production
-  // $path = "../../../INFO3/S6/PW6"; //path local
+require_once '../../php/php-markdown-lib/Michelf/Markdown.php';
 
-  if(!isset($_GET["tp"])){
-    $_GET["tp"] = "1";
-  }
-  switch($_GET["prof"]){
-    case "allegre":
-      $path .= "/1-Allegre/Exercices/TP".$_GET["tp"]."/compte-rendu.md";
-      break;
-    case "pittion":
-      $path .= "/2-Pittion/Exercices/TP".$_GET["tp"]."/compte-rendu.md";
-      break;
-    default:
-      $path .= "/2-Pittion/Exercices/TP1/compte-rendu.md";
-      break;
-  }
-  echo $path;
-  $txt = file_get_contents($path);
-  echo $txt;
-  $html = Markdown::defaultTransform($txt);
-  ?>
+spl_autoload_register(function ($class) {
+  require str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')) . '.php';
+});
+
+use Michelf\Markdown;
+
+$path = "../../../PW6"; //path production
+// $path = "../../../INFO3/S6/PW6"; //path local
+
+if (!isset($_GET["tp"])) {
+  $_GET["tp"] = "1";
+}
+switch ($_GET["prof"]) {
+  case "allegre":
+    $path .= "/1-Allegre/Exercices/TP" . $_GET["tp"] . "/compte-rendu.md";
+    break;
+  case "pittion":
+    $path .= "/2-Pittion/Exercices/TP" . $_GET["tp"] . "/compte-rendu.md";
+    break;
+  default:
+    $path .= "/2-Pittion/Exercices/TP1/compte-rendu.md";
+    break;
+}
+echo $path;
+$txt = file_get_contents($path);
+echo $txt;
+$html = Markdown::defaultTransform($txt);
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -33,7 +36,7 @@ require_once '../../php/php-markdown-lib/Michelf/Markdown.inc.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Compte Rendu <?php echo(isset($_GET["tp"]) && isset($_GET["prof"]) ? "TP" . $_GET["tp"] . " - " . $_GET["prof"] :"TP1 - Pittion");?></title>
+  <title>Compte Rendu <?php echo (isset($_GET["tp"]) && isset($_GET["prof"]) ? "TP" . $_GET["tp"] . " - " . $_GET["prof"] : "TP1 - Pittion"); ?></title>
   <link rel="stylesheet" type="text/css" href="css/compte_rendu.css">
 </head>
 
