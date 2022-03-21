2a7400fef9a2c27d073043e1f41ee6b316e740c2
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-21 16:22:53 +0100

    Déboggage

--- info3/pw6/compte_rendu.php
+++ info3/pw6/compte_rendu.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Compte Rendu <?php echo(isset($_GET["tp"]) && isset($_GET["prof"]) ? "TP" . $_GET["tp"] . " - " . $_GET["prof"] :"TP1 - Pittion");?></title>
-  <link rel="stylesheet" type="text/css" href="css/compte_rendu.css">
-</head>
-
-<body>
-  <?php
-  require_once '../../php/php-markdown-lib/Michelf/Markdown.inc.php';
-
+<?php
+require_once '../../php/php-markdown-lib/Michelf/Markdown.inc.php';
+  spl_autoload_register(function($class){
+    require str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+  });
+  use Michelf\Markdown;
   $path = "../../../PW6"; //path production
   // $path = "../../../INFO3/S6/PW6"; //path local
 
@@ -31,10 +22,24 @@
       break;
   }
   echo $path;
-  // $txt = file_get_contents($path);
-  // echo $txt;
-  // $html = \Michelf\Markdown::defaultTransform($txt);
-  // echo $html;
+  $txt = file_get_contents($path);
+  echo $txt;
+  $html = Markdown::defaultTransform($txt);
+  ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Compte Rendu <?php echo(isset($_GET["tp"]) && isset($_GET["prof"]) ? "TP" . $_GET["tp"] . " - " . $_GET["prof"] :"TP1 - Pittion");?></title>
+  <link rel="stylesheet" type="text/css" href="css/compte_rendu.css">
+</head>
+
+<body>
+  <?php
+  echo $html;
   ?>
 </body>
 
