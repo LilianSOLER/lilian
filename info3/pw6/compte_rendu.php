20bbb94170cf526572f6f90bb71155c24d22580b
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-21 11:19:14 +0100

    Ajout page compte rendu et son css, modification des liens vers PW

---
+++ info3/pw6/compte_rendu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Compte Rendu <?php echo(isset($_GET["tp"]) && isset($_GET["prof"]) ? "TP" . $_GET["tp"] . " - " . $_GET["prof"] :"TP1 - Pittion");?></title>
+  <link rel="stylesheet" type="text/css" href="css/compte_rendu.css">
+</head>
+
+<body>
+  <?php
+  require_once '../../php/php-markdown-lib/Michelf/Markdown.inc.php';
+  use Michelf\Markdown;
+
+  $path = "../../../INFO3/S6/PW6";
+  if(!isset($_GET["tp"])){
+    $_GET["tp"] = "1";
+  }
+  switch($_GET["prof"]){
+    case "allegre":
+      $path .= "/1-Allegre/Exercices/TP".$_GET["tp"]."/compte-rendu.md";
+      break;
+    case "pittion":
+      $path .= "/2-Pittion/Exercices/TP".$_GET["tp"]."/compte-rendu.md";
+      break;
+    default:
+      $path .= "/2-Pittion/Exercices/TP1/compte-rendu.md";
+      break;
+  }
+
+  echo Markdown::defaultTransform(file_get_contents($path));
+  ?>
+</body>
+
+</html>
--- info3/pw6/index.php
+++ info3/pw6/index.php
@@ -17,38 +17,42 @@
             <h1>Correction TD de Programmation Web : </h1>
             <ul class="grille">
                 <div class="div1">
-                    <li>TD n°1 : Exercices PHP </li>
+                    <li>Allegre - TD n°1 : Exercices PHP </li>
                     <ul class='sous-html'>
-                        <li><a href="../../../PW6/Exercices/TP1/calcul.html">Calcul d'intérêts composés (niv. 1-2)</a></li>
-                        <li><a href="../../../PW6/Exercices/TP1/">Un peu de style en CSS (niv. 1)</a></li>
-                        <li><a href="../../../PW6/Exercices/TP1/multiplication.php">Table de multiplication (niv. 1-2)</a></li>
-                        <li><a href="../../../PW6/Exercices/TP1/unicode.php">Analyse des Caractères Unicode (niv. 2)</a></li>
-                        <li><a href="../../../PW6/Exercices/TP1/multiplication_with_generic_table.php">Affichage d'un tableau générique (niv. 2)</a></li>
-                        <li><a href="../../../PW6/Exercices/TP1/form_agenda.html">Calendrier - agenda web (niv. 3)</a></li>
-                        <li><a href="../../../PW6/Exercices/TP1/form_agenda_with_generic_table.php">Calendrier - agenda web (niv. 3)</a></li>
+                        <li><a href="compte_rendu.php?tp=1&prof=allegre">Compte Rendu</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/calcul.html">Calcul d'intérêts composés (niv. 1-2)</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/">Un peu de style en CSS (niv. 1)</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/multiplication.php">Table de multiplication (niv. 1-2)</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/unicode.php">Analyse des Caractères Unicode (niv. 2)</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/multiplication_with_generic_table.php">Affichage d'un tableau générique (niv. 2)</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/form_agenda.html">Calendrier - agenda web (niv. 3)</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP1/form_agenda_with_generic_table.php">Calendrier - agenda web (niv. 3)</a></li>
                         <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
                     </ul>
                 </div>
                 <div class="div2">
-                    <li>TD n°2 : Service Web </li>
+                    <li>Allegre - TD n°2 : Service Web </li>
                     <ul class='sous-html'>
-                        <li><a href="../../../PW6/Exercices/TP2/compte-rendu.md">Compte Rendu</a></li>
-                        <li><a href="../../../PW6/Exercices/TP2/Point_Acces_Wifi/comptage.php">Points d'accès wifi</a></li>
+                        <li><a href="compte_rendu.php?tp=2&prof=allegre">Compte Rendu</a></li>
+                        <li><a href="../../../PW6/1-Allegre/Exercices/TP2/Point_Acces_Wifi/comptage.php">Points d'accès wifi</a></li>
                         <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
                     </ul>
                 </div>
                 <div class="div3">
-                    <li>TD n°3 : Podcasts- TMBD</li>
+                    <li>Allegre - TD n°3 : Podcasts- TMBD</li>
                     <ul class='sous-html'>
-                        <li><a href="../../../PW6/Exercices/TP3/compte-rendu.md">Compte Rendu</a></li>
+                        <li><a href="compte_rendu.php?tp=3&prof=allegre">Compte Rendu</a></li>
                         <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
                     </ul>
                 </div>
                 <div class="div4">
-                    <li>TD n°4 : ???</li>
-                    <ul class='sous-html'>
-                        <li><a href="#">??</a></li>
-                    </ul>
+                    <li>Pittion - TD n°1 : HTML, CSS
+                    <li>
+                        <ul class='sous-html'>
+                            <li><a href="compte_rendu.php?tp=1&prof=pittion">Compte Rendu</a></li>
+                            <li><a href="../../../PW6/2-Pittion/Exercices/TP1/ex.html">Exercice 3</a></li>
+                            <li><a href="https://github.com/LilianSOLER/PW6/archive/refs/heads/master.zip">Télécharger le dossier</a></li>
+                        </ul>
                 </div>
             </ul>
         </div>
