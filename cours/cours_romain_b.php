35196ec3bd5358c1bb3836dc44595b8e6a6f602a
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-30 19:07:09 +0200

    Ajout cours 30/03 Romain b

--- cours/cours_romain_b.php
+++ cours/cours_romain_b.php
@@ -25,6 +25,7 @@
                         <li><a href=media/image/cours_16_03_romain_b.pdf>16 - Exo&Cours</a></li>
                         <li><a href=media/image/cours_23_03_romain_b.pdf>23 - Exo&Cours - Brevet 2019</a></li>
                         <li><a href="media/image/sujet-brevet-mathematiques-2019_fiche-annale.pdf">Sujet Brevet 2019</a></li>
+                        <li><a href=media/image/cours_30_03_romain_b.pdf>30 - Exo & Cours - Proba</a></li>
                     </ul>
                 </div>
             </ul>
